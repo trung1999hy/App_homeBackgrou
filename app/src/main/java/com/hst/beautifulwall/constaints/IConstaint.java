459c8515b6f2fe0f43706cb16e085568c6984956
trung1999hy <trantrung1999hy@gmail.com>
2023-06-08 08:15:40 +0700

    trungtv

--- app/src/main/java/com/hst/beautifulwall/constaints/IConstaint.java
+++ app/src/main/java/com/hst/beautifulwall/constaints/IConstaint.java
@@ -1,14 +1,14 @@
 package com.hst.beautifulwall.constaints;
 
 public interface IConstaint {
-    public static final String KEY_1_COIN = "key_in_app_1"; // 50 coin
-    public static final String KEY_2_COIN = "key_in_app_2"; // 100 coin
-    public static final String KEY_3_COIN = "key_in_app_3"; // 200 coin
-    public static final String KEY_4_COIN = "key_in_app_4"; // 400 coin
-    public static final String KEY_5_COIN = "key_in_app_5"; // 600 coin
-    public static final String KEY_6_COIN = "key_in_app_6"; // 700 coin
+    public static final String KEY_1_COIN = "key_in_app_8"; // 50 coin
+    public static final String KEY_2_COIN = "key_in_app_9"; // 100 coin
+    public static final String KEY_3_COIN = "key_in_app_10"; // 200 coin
+    public static final String KEY_4_COIN = "key_in_app_11"; // 400 coin
+    public static final String KEY_5_COIN = "key_in_app_12"; // 600 coin
+    public static final String KEY_6_COIN = "key_in_app_13"; // 700 coin
 
-    public static final String KEY_7_COIN = "key_in_app_7"; // 99 coin
+    public static final String KEY_7_COIN = "key_in_app_14"; // 99 coin
 
     String COIN_USED_RESULT = "coin_used_result";
     String COIN_ORDER_RESULT = "coin_order_result";
